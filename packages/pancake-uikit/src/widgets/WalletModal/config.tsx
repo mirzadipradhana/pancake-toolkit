0e975695644f4cf5c30cb14708eea03018f3e120
Mirza Adipradhana <madipradhana@gmail.com>
2022-02-08 23:08:59 +0800

    feat: add DefiWallet

--- packages/pancake-uikit/src/widgets/WalletModal/config.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/config.tsx
@@ -1,11 +1,12 @@
 import Metamask from "../../components/Svg/Icons/Metamask";
-import WalletConnect from "../../components/Svg/Icons/WalletConnect";
-import TrustWallet from "../../components/Svg/Icons/TrustWallet";
-import MathWallet from "../../components/Svg/Icons/MathWallet";
-import TokenPocket from "../../components/Svg/Icons/TokenPocket";
-import BinanceChain from "../../components/Svg/Icons/BinanceChain";
-import SafePal from "../../components/Svg/Icons/SafePal";
-import Coin98 from "../../components/Svg/Icons/Coin98";
+import DefiWallet from "../../components/Svg/Icons/DefiWallet";
+// import WalletConnect from "../../components/Svg/Icons/WalletConnect";
+// import TrustWallet from "../../components/Svg/Icons/TrustWallet";
+// import MathWallet from "../../components/Svg/Icons/MathWallet";
+// import TokenPocket from "../../components/Svg/Icons/TokenPocket";
+// import BinanceChain from "../../components/Svg/Icons/BinanceChain";
+// import SafePal from "../../components/Svg/Icons/SafePal";
+// import Coin98 from "../../components/Svg/Icons/Coin98";
 
 import { Config, ConnectorNames } from "./types";
 
@@ -17,48 +18,48 @@ const connectors: Config[] = [
     priority: 1,
   },
   {
-    title: "WalletConnect",
-    icon: WalletConnect,
-    connectorId: ConnectorNames.WalletConnect,
+    title: "DeFi Wallet Crypto.com",
+    icon: DefiWallet,
+    connectorId: ConnectorNames.DefiWallet,
     priority: 2,
   },
-  {
-    title: "Trust Wallet",
-    icon: TrustWallet,
-    connectorId: ConnectorNames.Injected,
-    priority: 3,
-  },
-  {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-  },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-  },
+  // {
+  //   title: "Trust Wallet",
+  //   icon: TrustWallet,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 3,
+  // },
+  // {
+  //   title: "MathWallet",
+  //   icon: MathWallet,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  // },
+  // {
+  //   title: "TokenPocket",
+  //   icon: TokenPocket,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  // },
 
-  {
-    title: "Binance Chain",
-    icon: BinanceChain,
-    connectorId: ConnectorNames.BSC,
-    priority: 999,
-  },
-  {
-    title: "SafePal",
-    icon: SafePal,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-  },
-  {
-    title: "Coin98",
-    icon: Coin98,
-    connectorId: ConnectorNames.Injected,
-    priority: 999,
-  },
+  // {
+  //   title: "Binance Chain",
+  //   icon: BinanceChain,
+  //   connectorId: ConnectorNames.BSC,
+  //   priority: 999,
+  // },
+  // {
+  //   title: "SafePal",
+  //   icon: SafePal,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  // },
+  // {
+  //   title: "Coin98",
+  //   icon: Coin98,
+  //   connectorId: ConnectorNames.Injected,
+  //   priority: 999,
+  // },
 ];
 
 export default connectors;
