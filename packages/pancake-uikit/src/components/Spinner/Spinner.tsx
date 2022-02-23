bc7f2c094c960f5b4d7b75f6bda8f191545edb7b
Mirza <adipradhana@sicepat.com>
2022-02-24 02:44:15 +0800

    fix: adjust spinner component

--- packages/pancake-uikit/src/components/Spinner/Spinner.tsx
+++ packages/pancake-uikit/src/components/Spinner/Spinner.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
+import SpinnerIcon from "./SpinnerIcon";
 import { SpinnerProps } from "./types";
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
 
 const float = keyframes`
 	0% {
@@ -29,15 +19,7 @@ const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  animation: ${rotate} 2s linear infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
-const FloatingPanIcon = styled(PanIcon)`
+const FloatingSpinnerIcon = styled(SpinnerIcon)`
   animation: ${float} 6s ease-in-out infinite;
   transform: translate3d(0, 0, 0);
 `;
@@ -45,8 +27,7 @@ const FloatingPanIcon = styled(PanIcon)`
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <FloatingSpinnerIcon width={`${size}px`} />
     </Container>
   );
 };
